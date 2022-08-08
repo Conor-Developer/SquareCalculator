979781a40f7cd38cd401fc87cb87f2c53e5032fb
Conor-Developer <c_hope@hotmail.co.uk>
2022-08-08 15:56:30 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        double Width = 13.5;
+        double Height = 25.3;
+        System.out.println(Width);
+        System.out.println(Height);
+
+        // Calculates area using width and height
+        double Area = Width * Height;
+        System.out.println(Area);
+
+        // Calculates area using width and height
+        double Perimeter = (Width * 2) + (Height * 2);
+        System.out.println(Perimeter);
+
+    }
+}
